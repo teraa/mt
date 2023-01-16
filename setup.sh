200c33643229fca6b7fbca8784a0e30a2b013652
Tera <24725862+teraa@users.noreply.github.com>
2023-01-16 16:47:36 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ ip address add dev mt 10.20.0.1/24
 ip link set dev mt up
 # ip link set dev mt mtu 1212
 # ip link del dev mt
+# sudo setcap CAP_NET_RAW,CAP_NET_ADMIN=eip $(readlink -f $(which python3.11))
