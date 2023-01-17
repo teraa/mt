80d83cb9b768ad95f0c9a88e05644ce8864a0add
Tera <24725862+teraa@users.noreply.github.com>
2023-01-17 23:48:25 +0100

    add icmp.py

---
+++ icmp.py
@@ -0,0 +1,10 @@
+import socket
+from scapy.layers.inet import *
+
+print('Start')
+with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
+    # sock.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
+    while True:
+        data, addr = sock.recvfrom(65536)
+        packet = IP(data)
+        print(f'{addr}: {packet}')
