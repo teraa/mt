db99ee7319ceab6802ad6ceb575cf6b49f72a230
Tera <24725862+teraa@users.noreply.github.com>
2023-01-22 19:28:35 +0100

    add reader thread

--- tcpnoise.py
+++ tcpnoise.py
@@ -1,6 +1,7 @@
 import logging
 import optparse
 import socket
+import threading
 import time
 import sighandler
 
@@ -17,12 +18,28 @@ def main():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((opt.host, opt.port))
-        i = 0
-        while True:
-            logging.debug(i)
-            sock.sendall(str.encode(str(i) + '\n'))
-            i = i + 1
-            time.sleep(opt.interval)
+        
+        def writer():
+            i = 0
+            while True:
+                logging.info(f'send: {i}')
+                sock.sendall(str.encode(str(i) + '\n'))
+                i = i + 1
+                time.sleep(opt.interval)
+        
+        def reader():
+            while True:
+                data = sock.recv(65535)
+                logging.info(f'recv: {bytes.decode(data).strip()}')
+        
+        w = threading.Thread(target=writer, name='Writer', daemon=True)
+        r = threading.Thread(target=reader, name='Reader', daemon=True)
+
+        w.start()
+        r.start()
+
+        w.join()
+        r.join()
 
 
 if __name__ == '__main__':
