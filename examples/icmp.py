e742dae2f6311c8f23b1a0a8abcc8bc353113f60
Tera <24725862+teraa@users.noreply.github.com>
2023-01-22 17:10:25 +0100

    update icmp example

--- examples/icmp.py
+++ examples/icmp.py
@@ -4,8 +4,13 @@ from scapy.layers.inet import *
 
 print('Start')
 with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
+# with socket.socket(socket.AF_PACKET, socket.SOCK_RAW) as sock:
+#     sock.bind(('enp0s3', 0x0800))
+    # sock.setsockopt(socket.SOL_IP, 1, 0)
     # sock.setsockopt(socket.SOL_IP, socket.IP_HDRINCL, 1)
     while True:
         data, addr = sock.recvfrom(65536)
         packet = IP(data)
+        # if ICMP not in packet:
+        #     continue
         print(f'{addr}: {packet}')
