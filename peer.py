e74d516ddf81bac6f79f7d950f9557426f27f4c3
Tera <24725862+teraa@users.noreply.github.com>
2023-01-25 20:57:43 +0100

    TunPeer inherit from TransportClient

--- peer.py
+++ peer.py
@@ -5,7 +5,7 @@ import pytun
 from transport import *
 
 
-class TunPeer(object):
+class TunPeer(TransportClient):
 
     def __init__(self, interface: str, address: str, netmask: str, mtu: int, client: TransportClient):
         tun = pytun.TunTapDevice(name=interface, flags=pytun.IFF_TUN | pytun.IFF_NO_PI)
@@ -18,45 +18,48 @@ class TunPeer(object):
         self._tun = tun
         self._client = client
 
-    def reader(self):
-        logging.debug('Start')
-        while True:
+    def tun_guard(func):
+        def tun_guard(*args):
             try:
-                data = self._tun.read(self._tun.mtu)
-                packet = IP(data)
-
-                if packet.proto == 2:
-                    logging.debug('Drop igmp')
-                    continue
-
-                if packet.proto == 128:
-                    logging.debug('Drop sscopmce')
-                    continue
-
-                if TCP in packet and packet[TCP].dport == 5355:
-                    logging.debug('Drop hostmon')
-                    continue
-
-                logging.debug(packet)
-                self._client.w.put(packet)
+                func(*args)
+                return True
 
             except pytun.Error as e:
                 if e[0] == errno.EINTR:
-                    continue
+                    return True
 
-    def writer(self):
-        logging.debug('Start')
-        while True:
-            try:
-                packet = self._client.r.get()
+                logging.exception(e)
+                raise
 
-                self._tun.write(raw(packet))
-                logging.debug(packet)
-                self._client.r.task_done()
+        return tun_guard
 
-            except pytun.Error as e:
-                if e[0] == errno.EINTR:
-                    continue
+    @tun_guard
+    def _read(self):
+        data = self._tun.read(self._tun.mtu)
+        packet = IP(data)
+
+        if packet.proto == 2:
+            logging.debug('Drop igmp')
+            return
+
+        if packet.proto == 128:
+            logging.debug('Drop sscopmce')
+            return
+
+        if TCP in packet and packet[TCP].dport == 5355:
+            logging.debug('Drop hostmon')
+            return
+
+        logging.debug(packet)
+        self._client.w.put(packet)
+
+    @tun_guard
+    def _write(self):
+        packet = self._client.r.get()
+
+        self._tun.write(raw(packet))
+        logging.debug(packet)
+        self._client.r.task_done()
 
     def run(self):
 
