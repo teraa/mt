4b73d11936dbe66de2401e07684a13cf0989c65d
Tera <24725862+teraa@users.noreply.github.com>
2023-06-04 20:21:47 +0200

    update parse.sh

--- benchmarks/parse.sh
+++ benchmarks/parse.sh
@@ -3,7 +3,10 @@
 set -e
 
 echo 'mode,send,recv'
-for file in *.json; do
-    mode=$(cut -d. -f1 <<< $file)
-    jq ".end | [.sum_sent, .sum_received] | [\"$mode\", (.[].bits_per_second / 1000 | round)] | @csv" -r $file
+
+for type in {tcp,udp}; do
+    for file in *$type.json; do
+        mode=$(cut -d. -f1 <<< $file)
+        jq ".end | [.sum_sent, .sum_received] | [\"$mode\", (.[].bits_per_second / 1000 | round)] | @csv" -r $file
+    done
 done
